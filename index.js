9c38ed7fe2c4503b437c590bb69c343d2c094398
Dani Mastandrea <dvmastandrea@gmail.com>
2021-12-19 01:47:23 -0300

    funcionando

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 
-btn1.addEventListener("click", calcular)
+btnCalcular.addEventListener("click", calcular)
 function calcular(){
     
     let bill=document.getElementById("inputBill").value;
     let people=document.getElementById("inputPeople").value;
-    let propina=document.getElementById("inputPropina")
+    let propina=document.getElementById("inputPropina").value;
     
     
     
     let a=bill/people
-    let b=a*15
+    let b=a*propina
     let c=b/100
     
     let d=a+c
